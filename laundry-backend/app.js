01262bed40773f66915267e9c2d1fbed54ac9eed
nikhila29 <54467649+nikhila29@users.noreply.github.com>
2021-11-03 14:00:38 +0530

    Update app.js

--- laundry-backend/app.js
+++ laundry-backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose  = require('mongoose')
-const PORT = 5000
+const PORT = 4000
 const {MONGOURI} = require('./keys')
 
 app.use(express.json())
