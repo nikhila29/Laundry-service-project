cfbb307707551ffb7eb6d20cbf1bd7b32d4f9ed7
kshitijpal123 <88338035+kshitijpal123@users.noreply.github.com>
2021-11-08 19:53:37 +0530

    front-end-complete-pop-up-left

--- laundry-frontend/src/components/dashboard/dash.js
+++ laundry-frontend/src/components/dashboard/dash.js
@@ -1,95 +1,124 @@
-import React from 'react';
-import "./dash.css"
-import {BrowserRouter as Router,Switch,Link,Route} from "react-router-dom";
-import user1 from "../../assets/img/user1.png"
- 
+import React from "react";
+import "./dash.css";
+import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
+import user1 from "../../assets/img/user1.png";
+
 export default function dashboard() {
   return (
-      <div className="container-fluid">
-        <div className="jumbotron">
-        <nav className="navbar navbar-expand-lg navbar-light solid-light">
-            <div>
-            <h3><a className="navbar-brand" href="#">LAUNDRY</a></h3>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+    <div className="container-fluid-body">
+      <div className="jumbotron bg-light border-bottom">
+        <nav className="navbar navbar-expand-lg">
+          <div className="collapse navbar-collapse float-left">
+            <h3 className="text-primary p-4">LAUNDRY</h3>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
             </button>
-            </div>
-            <div className="collapse navbar-collapse"id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Pricing <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Career</a>
-                    </li>
-                    <li className="nav-item">
-                        <img className="img-circle userimage" src={user1}  />
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">User Name</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-        </div>
-        <div class="button">
-            <div class="row justify-content-center">
-            <button type="submit" class="btn btn-primary">Create</button>
-        </div>
-        
-        <div class="row bg-#FFFFFF">
-        <div class="col-sm-auto bg-#FFFFFF sticky-top">
-            <div class="d-flex flex-sm-column flex-row flex-nowrap bg-light align-items-center sticky-top">
-                <a href="/" class="d-block p-3 link-dark text-decoration-none" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Icon-only">
-                    <i class="bi-bootstrap fs-1"></i>
-                </a>
-                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
-                            <i class="bi-house fs-1"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Dashboard">
-                            <i class="bi-speedometer2 fs-1"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Orders">
-                            <i class="bi-table fs-1"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Products">
-                            <i class="bi-heart fs-1"></i>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link py-3 px-2" title="" data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Customers">
-                            <i class="bi-people fs-1"></i>
-                        </a>
-                    </li>
-                </ul>
-                <div class="dropdown">
-                    <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="bi-person-circle h2"></i>
+          </div>
+          <div>
+            <div
+              className="collapse navbar-collapse float-right"
+              id="navbarNav"
+            >
+              <ul className="navbar-nav float-right">
+                <li className="nav-item active p-3">
+                  <a className="nav-link" href="#">
+                    Pricing <span class="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item p-3">
+                  <a className="nav-link" href="#">
+                    Career
+                  </a>
+                </li>
+                <div className="collapse navbar-collapse float-right p-3 bg-primary">
+                  <li>
+                    <img className="userimage rounded-circle" src={user1} />
+                  </li>
+                  <li>
+                    <a className="nav-link text-light" href="#">
+                      User Name
                     </a>
-                    <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                    </ul>
+                  </li>
+                  <li>
+                    <Link to={"/"}>
+                      <i class="bi bi-power fs-2 icon"></i>
+                    </Link>
+                  </li>
                 </div>
+              </ul>
             </div>
-        </div>
-        <div class="col-sm p-3 min-vh-100">
-            {/* <!-- content --> */}
-        </div>
-    </div>
-    <div class="footer">
-  <p>2021 © Laundry</p>
-</div>
-</div>
+          </div>
+        </nav>
       </div>
-  )
+      <div class="row align-items-center">
+        <div style={{ width: "6%" }} class="col-md-1  bg-primary">
+          <div class="row sidenav">
+            <div class="d-flex flex-sm-column flex-row flex-nowrap align-items-center sticky-top">
+              <div>
+                <ul class="nav nav-pills nav-flush flex-sm-column flex-row flex-nowrap mb-auto mx-auto text-center align-items-center">
+                  <li class="nav-item">
+                    <a
+                      href="/dashboard"
+                      class="nav-link py-3 px-2"
+                      title=""
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="right"
+                      data-bs-original-title="Home"
+                    >
+                      <i class="bi-house fs-2 icon"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/orders"
+                      class="nav-link py-3 px-2"
+                      title=""
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="right"
+                      data-bs-original-title="Dashboard"
+                    >
+                      <i class="bi-plus-circle-fill fs-2 icon"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/dashboard"
+                      class="nav-link py-3 px-2"
+                      title=""
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="right"
+                      data-bs-original-title="Orders"
+                    >
+                      <i class="bi-list-ul fs-2 icon"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-11">
+          <div className="text-center">
+            <p className="text-muted">No Orders Available</p>
+            <Link to={"/orders"}>
+              <button type="button" class="btn btn-primary">
+                Create
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div class="footer">
+        <p>2021 © Laundry</p>
+      </div>
+    </div>
+  );
 }
